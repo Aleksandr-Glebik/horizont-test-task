72ba807d1a85c09b7be9e20183c09cfa0d830bec
Aleksandr-Glebik <glebikaa@mail.ru>
2023-07-05 15:15:26 +0300

    feat: add img-s, add arr dataPhotos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
+import { dataPhotos } from './data/dataPhotos'
 
 function App() {
   return (
     <div>
-      init project
+      {
+        dataPhotos.map( obj => {
+          return (
+            <div key={obj.id}>
+              <img alt={obj.name} src={obj.path} />
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
---
+++ src/data/dataPhotos.js
@@ -0,0 +1,107 @@
+import imgWhiteDog from '../assets/img/dog-on-box.jpg'
+import imgWolf from '../assets/img/wolf.jpg'
+import imgJackRussellTerrier from '../assets/img/small-dog.jpg'
+import imgBrownLabrador from '../assets/img/brown-dog.jpg'
+import imgCatOnTheBlanket from '../assets/img/cat-under-the-blanket.jpg'
+import imgCatOnThePier from '../assets/img/cat-on-the-pier.jpg'
+import imgCatOnTheCouch from '../assets/img/cat-on-the-couch.jpg'
+import imgSea from '../assets/img/sea.jpg'
+import imgMountains from '../assets/img/mountains.jpg'
+import imgForest from '../assets/img/forest.jpg'
+import imgFog from '../assets/img/fog.jpg'
+import imgFlowers from '../assets/img/flowers.jpg'
+import imgField from '../assets/img/field.jpg'
+
+export const dataPhotos = [
+    {
+        id: 1,
+        name: 'Белый лабрадор',
+        description: 'Белый лабрадор в саду открывает посылку с Wildberis',
+        path: imgWhiteDog,
+        category: 'animal'
+    },
+    {
+        id: 2,
+        name: 'Волк',
+        description: 'Пара белых волков отдыхает на природе',
+        path: imgWolf,
+        category: 'animal'
+    },
+    {
+        id: 3,
+        name: 'Джек Рассел Терьр',
+        description: 'Джек Рассел Терьр позирует для постера фильма Маска',
+        path: imgJackRussellTerrier,
+        category: 'animal'
+    },
+    {
+        id: 4,
+        name: 'Коричниевый лабрадор',
+        description: 'Когда ты красивый пес, а твой хозяин фотограф на freelance, после 30 минут позирования в саду',
+        path: imgBrownLabrador,
+        category: 'animal'
+    },
+    {
+        id: 5,
+        name: 'Кот под одеялом',
+        description: 'Когда в пн утром звонит будильник, а тебе кажется, что сегодня суббота',
+        path: imgCatOnTheBlanket,
+        category: 'animal'
+    },
+    {
+        id: 6,
+        name: 'Кот на пристани',
+        description: 'Когда твой хозяин олигарх и ты идешь к нему на яхту',
+        path: imgCatOnThePier,
+        category: 'animal'
+    },
+    {
+        id: 7,
+        name: 'Кот на диване',
+        description: 'Когда к хозяйке приехала ее мама и зовет тебя к себе на ручки',
+        path: imgCatOnTheCouch,
+        category: 'animal'
+    },
+    {
+        id: 8,
+        name: 'Море',
+        description: 'Красивый закат на берегу моря',
+        path: imgSea,
+        category: 'nature'
+    },
+    {
+        id: 9,
+        name: 'Горы',
+        description: 'Горы и солнце',
+        path: imgMountains,
+        category: 'nature'
+    },
+    {
+        id: 10,
+        name: 'Лес',
+        description: 'Трапинка в лесу',
+        path: imgForest,
+        category: 'nature'
+    },
+    {
+        id: 11,
+        name: 'Туман',
+        description: 'Туман над полем',
+        path: imgFog,
+        category: 'nature'
+    },
+    {
+        id: 12,
+        name: 'Цветы',
+        description: 'Поле с цветами',
+        path: imgFlowers,
+        category: 'nature'
+    },
+    {
+        id: 13,
+        name: 'Поле',
+        description: 'Ржаное поле',
+        path: imgField,
+        category: 'nature'
+    },
+]
